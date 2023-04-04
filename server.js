900de6a828567d794fc23c19731c73277bce044e
bui thi diem <thidiembui092@gmail.com>
2023-04-04 23:33:19 +0700

    hoidanit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+//const express = require('express');
+import  express  from 'express';
+const path = require('path');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 app.get('/about', (req, res) => {
     res.send(`I'm Diem`)
